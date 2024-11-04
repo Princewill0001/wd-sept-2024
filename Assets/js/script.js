f49dd0706ae2098dcde7ab3c50d5dd958b1074ac
Princewill0001 <omorovie1234@gmail.com>
2024-11-04 10:40:01 +0100

    Add files via upload

---
+++ Assets/js/script.js
@@ -0,0 +1,11 @@
+let a = 6,
+    b = 5,
+    c = 8;
+let addScore = a + b + c
+let subScore = a - b - c
+let mulScore = a * b * c
+console.log(addScore,
+            subScore,
+            mulScore
+        );
+
